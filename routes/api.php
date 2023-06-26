54204f5d6565375ccc45cc0fae34fb5e81ae4a88
Daniel Alejandro Sanchez Paez <programazion1@gmail.com>
2023-06-26 11:18:46 -0500

    First endpoint

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+Route::get("/saludo",function(Request $request){
+    $message=['mensjae' =>"Hola mundo"];
+    return response()->json($message);
+
 });
